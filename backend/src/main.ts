34aabb01f590b3ab3db5bcacfb571d107208518d
5undaye <5undaye@naver.com>
2025-06-27 22:31:29 +0900

    chore: JWT 토큰 처리 및 스웨거 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('인프런 API 문서')
+    .setDescription('인프런 API 문서 설명입니다.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
