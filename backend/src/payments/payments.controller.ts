7b667f4d0477e9ff18466c1b187b4162f42f8364
5undaye <5undaye@naver.com>
2025-08-04 22:54:31 +0900

    feat: [BE] 포트원 SDK 연동 & 결제 검증 API 구현

---
+++ backend/src/payments/payments.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Headers, Logger, Post, Req, UseGuards } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { AccessTokenGuard } from '../auth/guards/access-token.guard';
+import { VerifyPaymentDto } from './dto/verify-payment.dto';
+import { Request } from 'express';
+
+@Controller('payments')
+export class PaymentsController {
+    private readonly logger = new Logger(PaymentsController.name);
+
+    constructor(private readonly paymentsService: PaymentsService) {}
+
+    @Post('verify')
+    @UseGuards(AccessTokenGuard)
+    async verifyPayment(@Body() verifyPaymentDto: VerifyPaymentDto, @Req() req: Request) {
+        return this.paymentsService.verifyPayment(verifyPaymentDto, req.user.sub);
+    }
+
+    @Post('webhook')
+    async handleWebhook(@Body() body: string, @Headers() headers: Record<string, string>) {
+        this.logger.log('Payment Webhook 받음');
+
+        return this.paymentsService.handleWebhook(body, headers);
+    }
+}
